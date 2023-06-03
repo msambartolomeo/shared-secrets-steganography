f38b90d9625ab6dc3c2af9f51236d98a7e53e03d
Manuel <mdizenhaus@itba.edu.ar>
2023-06-03 10:47:37 -0300

    small correction

--- src/steganography.c
+++ src/steganography.c
@@ -1,16 +1,16 @@
 #include "include/steganography.h"
 #include "include/bmp.h"
 #include <stdint.h>
+#include <stdlib.h>
 
-int hideShadowBytes(BmpImage * img, uint8_t *shadow, enum StegMode mode) {
+int hideShadowBytes(BmpImage *img, uint8_t *shadow, enum StegMode mode) {
     if (img == NULL || shadow == NULL) {
         return -1;
     }
 
     uint8_t *imgBytes = img->image;
-    int i = 0;
     int k = 0;
-    for (/*byte : shadow*/) { // TODO: Escribir esta condición
+    for (int i = 0; i < sizeof(shadow); i++) { // TODO: Chequear esta condición
         int *bits = getBitArray(*(shadow + i));
         if (mode == LSB2) {
             for (int j = 0; j <= 6; j += 2) {
@@ -27,30 +27,31 @@ int hideShadowBytes(BmpImage * img, uint8_t *shadow, enum StegMode mode) {
     return 0;
 }
 
-void setLSB2(uint8_t *byte, uint8_t bit1, uint8_t bit0){
+void setLSB2(uint8_t *byte, uint8_t bit1, uint8_t bit0) {
     unsigned char toInclude = 0x00;
-    
+
     toInclude |= (bit1 & 0x01) << 1; // 0000 00 0/1 0
-    toInclude |= (bit0 & 0x01); // 0000 00x0/1
+    toInclude |= (bit0 & 0x01);      // 0000 00x0/1
 
     *(byte) &= 0xFC;
-    *(byte) |= toInclude; 
+    *(byte) |= toInclude;
 }
 
-void setLSB4(uint8_t *byte, uint8_t bit3, uint8_t bit2, uint8_t bit1, uint8_t bit0) {
+void setLSB4(uint8_t *byte, uint8_t bit3, uint8_t bit2, uint8_t bit1,
+             uint8_t bit0) {
     unsigned char toInclude = 0x00;
-    
+
     toInclude |= (bit3 & 0x01) << 3;
     toInclude |= (bit2 & 0x01) << 2; // 0000 00 0/1 0
     toInclude |= (bit1 & 0x01) << 1; // 0000 00 0/1 0
-    toInclude |= (bit0 & 0x01); 
+    toInclude |= (bit0 & 0x01);
 
     *(byte) &= 0xF0;
-    *(byte) |= toInclude; 
+    *(byte) |= toInclude;
 }
 
 int *getBitArray(uint8_t byte) {
-    int bits[8];
+    int *bits = (int *)malloc(sizeof(int) * 8);
     for (int i = 7; i >= 0; i--) {
         bits[i] = (byte >> i) & 1;
     }
